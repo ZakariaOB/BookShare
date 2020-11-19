df36f949f1d0c200388090843b34b5f2db47d924
Boukhris Zakaria <boukhriszakaria@gmail.com>
2020-11-19 21:39:41 +0100

    Register and loging forms

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,25 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Book share app';
+  title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
-  ngOnInit(): void {
-    this.getUsers();
+  ngOnInit() {
+    this.setCurrentUser();
   }
 
-  getUsers(): void {
-    this.http
-      .get('https://localhost:5001/api/users')
-      .subscribe((items) => (this.users = items));
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
