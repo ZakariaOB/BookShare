d62a376f70cb1740a7ef387ded4dc369453a159e
ZakariaOB <boukhriszakaria@gmail.com>
2021-01-23 21:27:39 +0100

    Sandbox with dynamic components

---
+++ client/src/app/_sandbox/dynamic-components/component-holder.ts
@@ -0,0 +1,8 @@
+import { DynamicComponentEnum } from "./dynamic-component-enum";
+
+export class ComponentHolder {
+    constructor(
+        public text: string,
+        public enumCompnent: DynamicComponentEnum
+      ) {}
+}
