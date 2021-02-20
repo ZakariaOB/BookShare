4053282b14dc54839e2f279838545b88c0c9a1df
ZakariaOB <boukhriszakaria@gmail.com>
2021-02-20 19:16:37 +0100

    intiial commit : Edit user city

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -14,8 +14,9 @@ namespace API.Entities
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
+        public int CityId {get; set;}
+        public City City {get; set;}
+
         public ICollection<Photo> Photos { get; set; }
         public ICollection<UserLike> LikedByUsers { get; set; }
         public ICollection<UserLike> LikedUsers { get; set; }
